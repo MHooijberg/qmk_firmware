b468028dea39ff4778420147a87d7cff8e22a673
MHooijberg <m.hooijberg24@gmail.com>
2024-05-24 20:33:22 +0200

    Deleted unused keyboards

--- keyboards/keychron/v1/ansi_encoder/keymaps/custom/keymap.c
+++ keyboards/keychron/v1/ansi_encoder/keymaps/custom/keymap.c
@@ -14,6 +14,24 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+ /* Default keyboard layout:
+  * ┌───┐┌───┬───┬───┬───┐┌───┬───┬───┬───┐┌───┬───┬───┬───┐┌───┐ ┌───┐
+  * │Esc││F1 │F2 │F3 │F4 ││F5 │F6 │F7 │F8 ││F9 │F10│F11│F12││Prt│ │Enc│
+  * └───┘└───┴───┴───┴───┘└───┴───┴───┴───┘└───┴───┴───┴───┘└───┘ └───┘
+  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┐
+  * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │Bck Spc│ │Hom│
+  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┤
+  * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │ │Ins│
+  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ ├───┤
+  * │ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │ │Del│
+  * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─────┬──┘ └───┘
+  * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ Shift │┌───┐
+  * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴───┬───┘│ ↑ │
+  * │Ctrl│GUI │Alt │                        │Alt│FN │Ctrl│┌───┼───┼───┐
+  * └────┴────┴────┴────────────────────────┴───┴───┴────┘│ ← │ ↓ │ → │
+  *                                                       └───┴───┴───┘
+  */
+
 #include "quantum.h"
 #include QMK_KEYBOARD_H
 #include "print.h"
@@ -29,9 +47,9 @@ enum custom_keycodes {
 
 enum layers{
     DEFAULT,
-    Layer1,
-    Layer2,
-    FN
+    FN,
+    LAYER1,
+    LAYER2,
 };
 
 enum modes{
@@ -63,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_INS,
       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_DEL,
       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(3),  DF(GAMING), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(1),  DF(LAYER1), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
   // Layer used for functions.
   [FN] = LAYOUT_ansi_82(
       _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
@@ -73,31 +91,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
       _______,  _______,  _______,                                _______,                                _______,  _______,    _______,  _______,  _______,  _______),
   // Layer used for programming.
-  [Layer1] = LAYOUT_ansi_82( 
+  [LAYER1] = LAYOUT_ansi_82( 
       KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_PSCR,            ENC_CLCK,
       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_HOME,
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_INS,
       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_DEL,
       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(3),  DF(GAMING), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(1),  DF(LAYER2), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
   // Layer used for gaming.
-  [Layer2] = LAYOUT_ansi_82(
+  [LAYER2] = LAYOUT_ansi_82(
       KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   A(KC_PSCR),         KC_MUTE,
       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_F13,
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_F14,
       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_F15,
       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(3),  DF(PROGRAMMING), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(1),  DF(DEFAULT), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-  [DEFAULT] = { ENCODER_CCW_CW(ENC_LFT, ENC_RGHT)},
-  [FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
-  [Layer1]   = { ENCODER_CCW_CW(ENC_LFT, ENC_RGHT)},
-  [Layer2] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+  [DEFAULT] = {ENCODER_CCW_CW(ENC_LFT, ENC_RGHT)},
+  [FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
+  [Layer1]   = {ENCODER_CCW_CW(ENC_LFT, ENC_RGHT)},
+  [Layer2] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif // ENCODER_MAP_ENABLE
+
+const uint16_t PROGMEM mode_map[][][] = {
+  [] = {}
+}
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
@@ -116,13 +138,21 @@ void matrix_scan_user(void) {
   if (is_enc_clck_active && timer_elapsed(enc_clck_timer) >= 300) {
     current_mode = (current_mode + 1) % NUM_OF_MODES; // Switch current encoder mode.
     is_enc_clck_active = false;
-    return false;
   }
 }
 
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
+        }
+    }
+    return false;
+}
+// Should be replaced by encoder_update_user
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
-
   switch (keycode) {
     case ENC_CLCK: 
       if (record->event.pressed && is_enc_clck_active == false)
@@ -212,7 +242,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           break;
       }
       return false;
-    }
+  }
     return true;
 }
 
